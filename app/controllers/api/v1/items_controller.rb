162b1390cdc00b1a753e0aaa98e08cf07d338486
Xin shen <363088847@qq.com>
2024-04-26 00:05:34 +0800

    完成 /api/v1/items 筛选功能

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.page(params[:page])
+    items = Item.where({created_at: params[:created_after]..params[:created_before]})
+      .page(params[:page])
     render json: { resources: items, pager: {
       page: params[:page],
       per_page: 100,
@@ -9,7 +10,7 @@ class Api::V1::ItemsController < ApplicationController
   end
   def create
     item = Item.new amount: params[:amount]
-  if item.save
+    if item.save
       render json: {resource: item}
     else
       render json: {errors: item.errors}
